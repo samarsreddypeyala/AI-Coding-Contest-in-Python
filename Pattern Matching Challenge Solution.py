504ab236d53e47e0fa157b0225c5bbd10b313f1b
Peyala Samarasimha Reddy <samarasimhareddy369@gmail.com>
2020-05-14 13:32:35 +0530

    Add files via upload

---
+++ Pattern Matching Challenge Solution.py
@@ -0,0 +1,12 @@
+
+# string is already given to you. Please don't edit this string.
+
+str=input()
+
+stringData = "qwe sdf dsld dssdfsqwe sdlcsd fdslkcsdsdk sdvnsdnvs dsvd d d dddqwelkmvl sdlksf qwelkmdsldm dsfkmsdf ds lknvnv dsdfdfnoiewqwek sdjnsdf djndsjnnqwnewefsjdc kqwj fsdfjsldnlsqwelkwnekennlksnq dlkneknqwn wqenln qlwn qlwknr wkernwen dkfndks ewqsdkslf efwekwkewqwen mdfsdfsdfskdnlknqwenknfsd lsklksna kasndasndqweq we qkewkwj e kjnqwne sd kqjwnekjqnwda kjqwnej dajqkjwe k wd qwekqwle kjnwqkejqw qwe jqnwjnqw djwnejwd"
+
+print(stringData.count(str))
+
+#Taking string as an input and gives output as the count of occurences in integer format
+
+#end
